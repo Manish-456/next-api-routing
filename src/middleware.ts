d94010c69cf704335aa9e9cfc3ee5d46c51ad0d1
Manish-456 <tamangmanish3456@gmail.com>
2023-05-22 13:41:55 +0545

    middleware and limiter added

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+const allowedOrigins = process.env.NODE_ENV === 'production'
+ ? [`https://www.yoursite.com`, 'https://yoursite.vercel.app'] : ['http://localhost:3000']
+
+export function middleware(request: Request) {
+    // const regex: RegExp = new RegExp('/api/*');
+    // if (regex.test(request.url)) {
+
+    // }
+    const origin = request.headers.get('origin');
+   
+    if(origin && !allowedOrigins.includes(origin)) return new NextResponse(null, {
+        status : 400,
+        statusText : 'Bad Request',
+        headers : {
+            'Content-Type' : 'text/plain'
+        }
+    })
+
+
+
+
+
+
+
+   
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: '/api/:path*'
+
+}
